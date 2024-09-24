77072d8d23566c1577ccea8777ef246acd0ed493
wcxt <68016750+wcxt@users.noreply.github.com>
2024-09-24 18:38:59 +0200

    test: Add tests for hop-by-hop headers

--- proxy_test.go
+++ proxy_test.go
@@ -14,19 +14,45 @@ const (
 	TestProxyURL     = "http://localhost:8080"
 	TestStatusCode   = http.StatusAccepted
 	TestResponseBody = "Hello World!"
-	TestHeaderKey    = "Test-Header"
-	TestHeaderValue  = "testvalue"
 	TestProto        = "HTTP/1.1"
 )
 
 func TestHandleProxyRequest(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if len(r.TransferEncoding) != 0 {
+			t.Errorf("got Transfer-Encoding = %v, want %v", r.TransferEncoding, []string{})
+		}
+		if r.Header.Get("TE") != "trailers" {
+			t.Errorf("got TE = %s, want %s", r.Header.Get("TE"), "trailers")
+		}
+		if r.Header.Get("Connection") != "" {
+			t.Errorf("got Connection = %s, want %s", r.Header.Get("Connection"), "")
+		}
+		if r.Header.Get("Keep-Alive") != "" {
+			t.Errorf("got Keep-Alive = %s, want %s", r.Header.Get("Keep-Alive"), "")
+		}
+		if r.Header.Get("Upgrade") != "" {
+			t.Errorf("got Upgrade = %s, want %s", r.Header.Get("Upgrade"), "")
+		}
+		if len(r.Trailer) != 0 {
+			t.Errorf("got Trailer = %v, want %v", r.Trailer, http.Header{})
+		}
+		if r.Header.Get("Proxy-Authorization") != "" {
+			t.Errorf("got Proxy-Authorization = %s, want %s", r.Header.Get("Proxy-Authorization"), "")
+		}
+		if r.Header.Get("Proxy-Authenticate") != "" {
+			t.Errorf("got Proxy-Authenticate = %s, want %s", r.Header.Get("Proxy-Authenticate"), "")
+		}
+		if r.Header.Get("Remove-Header") != "" {
+			t.Errorf("got Remove-Header = %s, want %s", r.Header.Get("Remove-Header"), "")
+		}
+
 		want := TestProto + " " + gorp.ProxyPseudonym
 		if r.Header.Get("Via") != want {
 			t.Errorf("got Via = %s, want %s", r.Header.Get("Via"), want)
 		}
 
-		w.Header().Set(TestHeaderKey, TestHeaderValue)
+		w.Header().Set("Response-Header", "test")
 		w.WriteHeader(TestStatusCode)
 		w.Write([]byte(TestResponseBody))
 	}))
@@ -41,6 +67,15 @@ func TestHandleProxyRequest(t *testing.T) {
 	w := httptest.NewRecorder()
 	req := httptest.NewRequest("GET", TestProxyURL, nil)
 	req.Proto = TestProto
+	req.TransferEncoding = []string{"gzip", "chunked"}
+	req.Header.Set("TE", "trailers, deflate")
+	req.Header.Set("Connection", "close, Remove-Header")
+	req.Header.Set("Keep-Alive", "test")
+	req.Header.Set("Upgrade", "test")
+	req.Header.Set("Trailer", "Trailer-Header")
+	req.Header.Set("Proxy-Authorization", "test")
+	req.Header.Set("Proxy-Authenticate", "test")
+	req.Header.Set("Remove-Header", "test")
 
 	handle(w, req)
 	res := w.Result()
@@ -48,8 +83,8 @@ func TestHandleProxyRequest(t *testing.T) {
 	if res.StatusCode != TestStatusCode {
 		t.Errorf("got res.StatusCode = %d, want %d", res.StatusCode, TestStatusCode)
 	}
-	if res.Header.Get(TestHeaderKey) != TestHeaderValue {
-		t.Errorf("got Test-Header = %s, want %s", res.Header.Get(TestHeaderKey), TestHeaderValue)
+	if res.Header.Get("Response-Header") != "test" {
+		t.Errorf("got Test-Header = %s, want %s", res.Header.Get("Response-Header"), "test")
 	}
 
 	body, err := io.ReadAll(res.Body)
